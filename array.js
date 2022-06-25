32f4f4e4fc122383f1cb8b99b6febeb97de5f8a5
ILhankhondaker <ilhankhondaker@gmail.com>
2022-06-26 05:28:23 +0600

    add sum function

--- array.js
+++ array.js
@@ -1,8 +1,8 @@
 // let arr1 = [1, 4, 6, 6, 1, 8, 9, 10, 1]
-
-
 // let arr1 = [3, 'a', 'a', 3, 'a', 2, 3, 'a', 3, 3, 3, 'a', 2, 4, 9, 3];
-let arr1 = [1, 5, 1, 10]
+// let arr1 = ['py', 'js', 'js', 'c', 'js', 'c#', 'py', 'c']
+// let arr1 = [1, 4, 6, 6, 1, 8, 9, 10, 1]
+let arr1 = [1, 4, 10, 1]
 let mf = 1;
 let m = 0;
 let item;
@@ -18,3 +18,26 @@ for (let i = 0; i < arr1.length; i++) {
     m = 0;
 }
 console.log(item + " ( " + mf + " times ) ");
+
+//=== If we wanna need sum of arry all number =====
+const numbers = [1, 4, 6, 6, 1, 8, 9, 10, 1, 4];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    sum = sum + element;
+}
+// console.log("Total sum=", sum);
+
+
+//=== If we wanna need sum of arry all number to use function=====
+function arrayTotal(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+let numb = [1, 4, 6, 6, 1, 8, 9, 10, 1, 4, 50];
+const total = arrayTotal(numb);
+console.log('array total', total)
